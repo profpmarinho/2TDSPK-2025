c941da483f2f2974670692a4ab21dde4e2dd961b
Paulo Marinho <tommymolto@gmail.com>
2025-03-12 21:25:31 -0300

    aula 5

--- Aulas/aula-13-03-25/App.js
+++ Aulas/aula-13-03-25/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
+// Componente principal do aplicativo
 export default function App() {
   return (
     <View style={styles.container}>
-      <Classe />
+      {/* Renderiza o componente Classe */}
+      <Classe name="User" />
     </View>
   );
 }
 
+// Estilos para o componente principal
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,22 +22,33 @@ const styles = StyleSheet.create({
   },
 });
 
+// Componente de classe que estende React.PureComponent
 class Classe extends React.PureComponent {
-    state = {
-        titleText : 'Ninguem apertou o botao'
-    }
-    onPressButton = (name) => {
-        this.setState({titleText: `Botao pressionado por ${name}`})
-    }
+  // Estado inicial do componente
+  state = {
+    titleText: 'Ninguem apertou o botao'
+  }
+
+  // Função chamada quando o botão é pressionado
+  onPressButton = (name) => {
+    const now = new Date();
+    const timeString = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}:${now.getMilliseconds()}`;
+    this.setState({ titleText: `Botao pressionado por ${name} as ${timeString}` });
+  }
+
+  // Renderiza o componente
   render() {
     return (
       <View>
+        {/* Exibe o texto do estado */}
         <Text>
-        {this.state.titleText}
+          {this.state.titleText}
         </Text>
+        {/* Botão que chama a função onPressButton ao ser pressionado */}
         <Button title='Stop capturing'
-        onPress={() => this.onPressButton(this.props.name)}
-        color='#FF0000'></Button>
+          onPress={() => this.onPressButton(this.props.name)}
+          color='#FF0000'>
+        </Button>
       </View>
     );
   }
